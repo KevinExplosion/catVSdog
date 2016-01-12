75091a9f668ec5e930f9c70ba2001cf4798ac07a
kevin mattison and pablo vera <pablovera.pdx@gmail.com>
2016-01-12 10:11:15 -0800

    Add styling for button response images.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
  $("button#bark").click(function() {
    $("ul#dog").prepend("<img src=images/happyDog.jpg>");
    $("ul#cat").prepend("<img src=images/happyCat.jpg>");
+
+/*-----Sends an alert pop up to the user that clicking will remove the image-----*/
+/*   $("img").childrenclick(function() {
+     alert("This will remove the image!");
+   });
+/*
+/*------end alert-----*/
+
    $("ul#dog").children("img").first().click(function() {
     $(this).remove();
   });
@@ -14,6 +22,14 @@ $(document).ready(function() {
  $("button#growl").click(function() {
    $("ul#dog").prepend("<img src=images/angryDog.jpg>");
    $("ul#cat").prepend("<img src=images/angryCat.jpg>");
+
+   /*-----Sends an alert pop up to the user that clicking will remove the image-----*/
+   /*   $("img").childrenclick(function() {
+        alert("This will remove the image!");
+      });
+   /*
+   /*------end alert-----*/
+
    $("ul#dog").children("img").first().click(function() {
      $(this).remove();
    });
@@ -26,6 +42,14 @@ $(document).ready(function() {
  $("button#wag").click(function() {
   $("ul#dog").prepend("<img src=images/waggingDog.jpg>");
   $("ul#cat").prepend("<img src=images/purringCat.jpg>");
+
+  /*-----Sends an alert pop up to the user that clicking will remove the image-----*/
+  /*   $("img").childrenclick(function() {
+       alert("This will remove the image!");
+     });
+  /*
+  /*------end alert-----*/
+
   $("ul#dog").children("img").first().click(function() {
     $(this).remove();
   });
