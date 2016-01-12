efdb8c3d5f12285f6ea95fa905bb0a12d12f424c
kevin mattison and pablo vera <mattisonii@yahoo.com>
2016-01-12 09:58:57 -0800

    add photos to buttons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,38 @@
 $(document).ready(function() {
  $("button#bark").click(function() {
-   $("ul#dog").prepend("<li>Bark bark!</li>");
-   $("ul#cat").prepend("<li>Meow meow!</li>");
-   $("li").click(function() {
-     alert("hi");
+   $("ul#dog").prepend("<img src=images/happyDog.jpg>");
+   $("ul#cat").prepend("<img src=images/happyCat.jpg>");
+   $("ul#dog").children("img").first().click(function() {
+    $(this).remove();
+  });
+
+  $("ul#cat").children("img").first().click(function() {
+    $(this).remove();
    });
  });
 
  $("button#growl").click(function() {
-   $("ul#dog").prepend("<li>Growl!</li>");
-   $("ul#cat").prepend("<li>Hiss!</li>");
-   $("li").click(function() {
-     alert("hi");
+   $("ul#dog").prepend("<img src=images/angryDog.jpg>");
+   $("ul#cat").prepend("<img src=images/angryCat.jpg>");
+   $("ul#dog").children("img").first().click(function() {
+     $(this).remove();
    });
+
+   $("ul#cat").children("img").first().click(function() {
+     $(this).remove();
+    });
  });
 
  $("button#wag").click(function() {
-  $("ul#dog").prepend("<li>*Wags tail*</li>");
-  $("ul#cat").prepend("<li>Purrrr</li>");
-  $("li").click(function() {
-    alert("hi");
-    });
+  $("ul#dog").prepend("<img src=images/waggingDog.jpg>");
+  $("ul#cat").prepend("<img src=images/purringCat.jpg>");
+  $("ul#dog").children("img").first().click(function() {
+    $(this).remove();
   });
+
+  $("ul#cat").children("img").first().click(function() {
+    $(this).remove();
+   });
+ });
 
 });
